dd94f59b03c23b37ea5ff301cf1b187a167d100f
debianmr <oriol@PCOriol>
2014-01-30 12:40:29 +0100

    [Pr] Es meva la demo... Només meva.

--- programa/demos/src/demo_3.cpp
+++ programa/demos/src/demo_3.cpp
@@ -9,6 +9,12 @@ bool Demo_3::prepare()
 	win.setFramerateLimit(60);
 	win.setVerticalSyncEnabled(true);
 	
+	sf::FloatRect scoreRect = car.shape.getLocalBounds();
+	car.shape.setOrigin(scoreRect.left + scoreRect.width / 2.f, scoreRect.top + scoreRect.height / 2.f);
+	car.shape.setPosition(100, 100);
+	
+	car.shape.setRotation(45);
+	
 	return true;
 }
 
@@ -39,6 +45,19 @@ void Demo_3::run()
 			}
 		}
 		
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+		{
+			car.shape.setRotation(car.shape.getRotation() + 1);
+		}
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+		{
+			car.shape.setRotation(car.shape.getRotation() -1);
+		}
+		
+		if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+		{
+			car.shape.getRotation 
+		}
 		win.clear(sf::Color(20, 20, 20));
 		win.draw(car.shape);
 		win.display();
