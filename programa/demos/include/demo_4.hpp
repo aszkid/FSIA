96958e9b6ffa70024e5aa1ea95aa29f9161e912f
potenshia <mferreoliver@gmail.com>
2014-03-24 12:27:15 +0100

    Demo 4

---
+++ programa/demos/include/demo_4.hpp
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "base.hpp"
+#include <cmath>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <ctime>
+#include <random>
+#include <array>
+/*#include <fann.h>
+#include <fann_cpp.h>*/
+
+class Demo_4 : public Demo {
+public:
+	bool prepare();
+	void run();
+private:
+	sf::RenderWindow win;
+	sf::Event eve;
+	
+	sf::Clock frameClock;
+	float frameTime;
+	
+	std::array<sf::RectangleShape, 16> grid;
+};
